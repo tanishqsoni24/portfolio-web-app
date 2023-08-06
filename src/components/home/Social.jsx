cbc1c21447fc28be45c98500709101a774376760
tanishqsoni0309 <tanishqsoni0309@gmail.com>
2023-08-06 20:41:06 +0530

    to-home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const social = () => {
+  return (
+    <div className="home__socials">
+        <a href="https://www.linkedin.com/in/tanishqsoni" target='_blank' className="home__social-icon">
+            <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://www.github.com/tanishqsoni24" target='_blank' className="home__social-icon">
+            <i class="uil uil-github-alt"></i>
+        </a>
+        
+    </div>
+  )
+}
+
+export default social
